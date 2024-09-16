8feaef4003927a466a1923488b2a1e3a89d0a115
Ian Parker <parker.ian.dev@gmail.com>
2024-09-17 08:04:01 +1000

    css sharebar scale fix

--- share-bar.js
+++ share-bar.js
@@ -8,7 +8,6 @@ shareButtons.forEach(btn => {
       shareBar.classList.remove("share-bar-open");
       shareArrowButton.style.backgroundColor = "hsl(210, 46%, 95%)";
       shareArrowButton.style.color = "hsl(217, 19%, 35%)";
-      console.log(shareArrowButton);
     } else {
       shareBar.classList.add("share-bar-open");
       shareArrowButton.style.backgroundColor = "hsl(217, 19%, 35%)";
